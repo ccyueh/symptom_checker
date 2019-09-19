08848ec057bf74558738c2c8202fe0005ceb220f
Christine Yueh <christinecyueh@gmail.com>
2019-09-19 01:24:40 -0400

    added api tests

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -10,65 +10,78 @@ def index():
 
 @app.route('/api/symptoms/retrieve', methods=['GET'])
 def getSymptoms():
-    results = db.session.query(Symptom).all()
-    symptoms = [(s.symptom_id, s.symptom_name.title()) for s in results]
-    
-    return jsonify({ 'symptoms': symptoms })
+    try:
+        results = db.session.query(Symptom).all()
+        symptoms = [(s.symptom_id, s.symptom_name.title()) for s in results]
+
+        return jsonify({ 'symptoms': symptoms })
+    except:
+        return jsonify({ 'error': 'No symptoms found.' })
 
 @app.route('/api/diagnoses/retrieve', methods=['GET'])
 def getDiagnosis():
-    symptom_id = request.args.get('symptom_id')
+    try:
+        symptom_id = request.args.get('symptom_id')
 
-    if symptom_id:
-        symptom = Symptom.query.filter_by(symptom_id=symptom_id).first()
-        if symptom:
-            records = db.session.query(Record.record_id, Record.symptom_id, Diagnosis.diagnosis_id, Diagnosis.diagnosis_name).join(Record).filter_by(symptom_id=symptom_id).all()
-            diagnoses = [(d.diagnosis_id,d.diagnosis_name.title()) for d in records]
-            diagnosis_counts = Counter(diagnoses)
-            top_diagnoses = diagnosis_counts.most_common()
-            if len(top_diagnoses) > 0:
-                diagnosis = top_diagnoses[0][0]
-                if len(top_diagnoses) > 1:
-                    alternatives = [d[0] for d in top_diagnoses[1:]]
+        if symptom_id:
+            symptom = Symptom.query.filter_by(symptom_id=symptom_id).first()
+            if symptom:
+                records = db.session.query(
+                    Record.record_id, 
+                    Record.symptom_id, 
+                    Diagnosis.diagnosis_id, 
+                    Diagnosis.diagnosis_name
+                ).join(Record).filter_by(symptom_id=symptom_id).all()
+                diagnoses = [(d.diagnosis_id,d.diagnosis_name.title()) for d in records]
+                diagnosis_counts = Counter(diagnoses)
+                top_diagnoses = diagnosis_counts.most_common()
+                if len(top_diagnoses) > 0:
+                    diagnosis = top_diagnoses[0][0]
+                    if len(top_diagnoses) > 1:
+                        alternatives = [d[0] for d in top_diagnoses[1:]]
+                    else:
+                        alternatives = []
+
+                    frequency = {}
+                    for k, v in diagnosis_counts.items():
+                        frequency[k[1]] = v/len(diagnoses)
+
+                    return jsonify({
+                        'diagnosis': diagnosis,
+                        'alternatives': alternatives,
+                        'frequency': frequency
+                    })
                 else:
-                    alternatives = []
-
-                frequency = {}
-                for k, v in diagnosis_counts.items():
-                    frequency[k[1]] = v/len(diagnoses)
-    
-                return jsonify({ 
-                    'diagnosis': diagnosis, 
-                    'alternatives': alternatives, 
-                    'frequency': frequency 
-                })
+                    return jsonify({ 'error': 'No diagnosis found.' })
             else:
-                return jsonify({ 'error': 'No diagnosis found.' })
+                return jsonify({ 'error': 'Symptom not found.' })
         else:
-            return jsonify({ 'error': 'Symptom not found.' })
-    else:
-        return jsonify({ 'error': 'Symptom ID required.' })
+            return jsonify({ 'error': 'Symptom ID required.' })
+    except:
+        return jsonify({ 'error': 'No diagnoses found.' })
 
 @app.route('/api/diagnoses/save', methods=['POST'])
 def saveDiagnosis():
-    data = request.json
+    try:
+        data = request.json
 
-    symptom_id = data.get('symptom_id') 
-    diagnosis_id = data.get('diagnosis_id')
+        symptom_id = data.get('symptom_id')
+        diagnosis_id = data.get('diagnosis_id')
 
-    if symptom_id and diagnosis_id:
-        symptom = db.session.query(Symptom).filter_by(symptom_id=symptom_id).first()
-        diagnosis = db.session.query(Diagnosis).filter_by(diagnosis_id=diagnosis_id).first()
-        
-        if symptom and diagnosis:
-            record = Record(symptom_id=symptom_id, diagnosis_id=diagnosis_id)
-            
-            db.session.add(record)
-            db.session.commit()
+        if symptom_id and diagnosis_id:
+            symptom = db.session.query(Symptom).filter_by(symptom_id=symptom_id).first()
+            diagnosis = db.session.query(Diagnosis).filter_by(diagnosis_id=diagnosis_id).first()
 
-            return jsonify({ 'success': 'Diagnosis saved.' })
+            if symptom and diagnosis:
+                record = Record(symptom_id=symptom_id, diagnosis_id=diagnosis_id)
+
+                db.session.add(record)
+                db.session.commit()
+
+                return jsonify({ 'success': 'Diagnosis saved.' })
+            else:
+                return jsonify({ 'error': 'Symptom and/or diagnosis not found.' })
         else:
-            return jsonify({ 'error': 'Symptom and/or diagnosis not found.' })
-    else:
-        return jsonify({ 'error': 'Symptom and diagnosis IDs are required.' })
-
+            return jsonify({ 'error': 'Symptom and diagnosis IDs are required.' })
+    except:
+        return jsonify({ 'error': 'Could not save diagnosis.' })
